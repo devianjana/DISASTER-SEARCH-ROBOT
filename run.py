f618a1045c67a7b992d6f72d9c2aaa4741259084
devi anjana <devianjanaanoops@gmail.com>
2025-11-09 14:06:47 +0530

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import time
-from object_detector import ObjectDetector 
-VIDEO_SOURCE = 0 
-def main_simulation():
-    detector = ObjectDetector()
-    print("ObjectDetector initialized successfully.")
-    cap = cv2.VideoCapture(VIDEO_SOURCE)
-
